be4aff8ea468b8484d92b4c063bd38208c4c7048
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-02-14 10:31:48 +0300

    add some entities

---
+++ Sender/Entities/ValueRow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sender.Entities
+{
+    public class ValueRow
+    {
+        public DateTime LastModifiedDate { get; set; }
+        public string Status { get; set; }
+        public bool IsSended { get; set; }
+        public string MessageText { get; set; }
+        public string TgUser { get; set; }
+    }
+}
--- Sqllite/User.cs
+++ Sqllite/User.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public string PhoneNumber { get; set; }
         public string ChatId { get; set; }
     }
 }
