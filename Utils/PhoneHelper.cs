a7ef84431e763ac2000d1c74e20209c0b98584b1
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-02-15 20:02:07 +0300

    add get only numeric

--- Utils/PhoneHelper.cs
+++ Utils/PhoneHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Utils
@@ -7,6 +8,7 @@ namespace Utils
     {
         string Format(string phone);
         bool IsPhone(string phone);
+        string GetOnlyNumerics(string phone);
     }
     public class PhoneHelper : IPhoneHelper
     {
@@ -15,12 +17,17 @@ namespace Utils
             var phoneParser = new Regex(@"(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})");
             var format = "$1 ($2) $3-$4-$5";
             var res = phoneParser.Replace(phone, format);
+            res = Regex.Replace(res, @"(\+)", "");
             return res;
         }
 
         public bool IsPhone(string phone)
         {
             return Regex.IsMatch(phone, @"^((1-)?\d{3}-)?\d{3}-\d{4}$");
+        }
+
+        public string GetOnlyNumerics(string phone) {
+            return string.Join("", phone.Where(c => Char.IsDigit(c)).ToArray());
         }
     }
 }
