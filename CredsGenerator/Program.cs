c2395e3d95ccad459bd94bfbf4eb23f31bcef69b
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-03-18 23:53:07 +0300

    add creds generator

---
+++ CredsGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CredsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] scopes = { SheetsService.Scope.Spreadsheets };
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                var credPath = Path.Combine("credentials/sheets.googleapis.mytgbot.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
