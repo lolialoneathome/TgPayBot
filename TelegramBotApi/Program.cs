ca924c18d86fe9471e1ff4d4fd4eb2d59de31f2d
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-04-08 15:08:14 +0300

    add port

--- TelegramBotApi/Program.cs
+++ TelegramBotApi/Program.cs
@@ -20,6 +20,7 @@ namespace TelegramBotApi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:8443")
                 .Build();
     }
 }
