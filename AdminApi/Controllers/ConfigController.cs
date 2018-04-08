c41b7bd276cfb1d144d17aadc91b1abf60f8f75d
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-04-08 13:46:05 +0300

    add admin api

---
+++ AdminApi/Controllers/ConfigController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayBot.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace AdminApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConfigController : Controller
+    {
+        protected readonly IConfigService _configService;
+
+        public ConfigController(IConfigService configService) {
+            _configService = configService ?? throw new ArgumentNullException(nameof(configService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+            try
+            {
+                return Ok(_configService.Config);
+            }
+            catch (Exception err) {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Config config) {
+            try
+            {
+                await _configService.UpdateConfig(config);
+                return NoContent();
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+    }
+}
