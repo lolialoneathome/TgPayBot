2c77c097ad1a523c59e4c057e2c4cd9fe22c5aae
Lolialoneathome <ekaterinabakuleva@gmail.com>
2018-04-13 00:16:25 +0300

    async logger + add log to file if goole table log fail

--- Utils/Logger/IBotLogger.cs
+++ Utils/Logger/IBotLogger.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Utils.Logger
 {
     public interface IBotLogger
     {
-        void LogSended(string action, string user);
-        void LogIncoming(string action, string user);
-        void LogSystem(string action, string user);
-        void LogError(string error);
-        void LogErrorList(IEnumerable<string> errors);
-        void LogSendedList(IEnumerable<SendedMessage> errors);
-        void LogAuth(string auth_message, string user);
+        Task LogSended(string action, string user);
+        Task LogIncoming(string action, string user);
+        Task LogSystem(string action, string user);
+        Task LogError(string error);
+        Task LogErrorList(IEnumerable<string> errors);
+        Task LogSendedList(IEnumerable<SendedMessage> errors);
+        Task LogAuth(string auth_message, string user);
     }
 }
--- Utils/Logger/ToGoogleTableBotLogger.cs
+++ Utils/Logger/ToGoogleTableBotLogger.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
+using Microsoft.Extensions.Logging;
 using PayBot.Configuration;
 using System;
 using System.Collections.Generic;
@@ -12,36 +13,70 @@ namespace Utils.Logger
     {
         private readonly IConfigService _configService;
         private readonly ISheetsServiceProvider _sheetServiceProvider;
-
+        protected readonly ILogger<ToGoogleTableBotLogger> _toFileLogger;
         public ToGoogleTableBotLogger(IConfigService configService, ISheetsServiceProvider sheetServiceProvider) {
             _configService = configService ?? throw new ArgumentNullException(nameof(configService));
             _sheetServiceProvider  = sheetServiceProvider ?? throw new ArgumentNullException(nameof(sheetServiceProvider));
         }
 
-        public void LogSended(string action, string user)
+        public async Task LogSended(string action, string user)
         {
-            LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.Outgoing);
+            try {
+                await LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.Outgoing);
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE. TYPE: SENDED. ACTION: {action} USER: {user} ERROR: {err.Message}");
+            }
         }
 
 
-        public void LogAuth(string auth_message, string user)
+        public async Task LogAuth(string auth_message, string user)
         {
-            LogToTable(_configService.Config.SpreadsheetLog.Auths, auth_message, user);
+            try
+            {
+                await LogToTable(_configService.Config.SpreadsheetLog.Auths, auth_message, user);
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE. TYPE: AUTH. AUTH_MSG: {auth_message} USER: {user} ERROR: {err.Message}");
+            }
         }
 
-        public void LogError(string error)
+        public async Task LogError(string error)
         {
-            LogToTable(_configService.Config.SpreadsheetLog.Errors, error);
+            try
+            {
+                await LogToTable(_configService.Config.SpreadsheetLog.Errors, error);
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE. TYPE: ERROR. ERROR_STR: {error} ERROR: {err.Message}");
+            }
         }
 
-        public void LogIncoming(string action, string user)
+        public async Task LogIncoming(string action, string user)
         {
-            LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.Incoming);
+            try
+            {
+                await LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.Incoming);
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE. TYPE: INCOMING. ERROR_STR: {action} USER: {user} ERROR: {err.Message}");
+            }
         }
 
-        public void LogSystem(string action, string user)
+        public async Task LogSystem(string action, string user)
         {
-            LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.System);
+            try
+            {
+                await LogToTable(_configService.Config.SpreadsheetLog.Messages, action, user, MessageType.System);
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE. TYPE: SYSTEM. ERROR_STR: {action} USER: {user} ERROR: {err.Message}");
+            }
         }
 
         private async Task LogToTable(string listname, string message, string user = null, MessageType? messageType = null) {
@@ -72,47 +107,60 @@ namespace Utils.Logger
             AppendValuesResponse response = await request.ExecuteAsync();
         }
 
-        public void LogErrorList(IEnumerable<string> errors)
+        public async Task LogErrorList(IEnumerable<string> errors)
         {
-            var service = _sheetServiceProvider.GetService().Result;
+            try {
+                var service = await _sheetServiceProvider.GetService();
 
-            var range = $"{_configService.Config.SpreadsheetLog.Errors}!A:B";
-            SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum valueInputOption
-                = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
-            SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum insertDataOption
-                = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
-            ValueRange valueRange = new ValueRange();
-            var oblist = errors.Select(x => (IList<object>) new List<object>() { string.Format(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")), x}).ToList();
-            valueRange.Values = oblist;
+                var range = $"{_configService.Config.SpreadsheetLog.Errors}!A:B";
+                SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum valueInputOption
+                    = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+                SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum insertDataOption
+                    = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+                ValueRange valueRange = new ValueRange();
+                var oblist = errors.Select(x => (IList<object>)new List<object>() { string.Format(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")), x }).ToList();
+                valueRange.Values = oblist;
 
 
-            SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(valueRange, _configService.Config.SpreadsheetLog.Id, range);
-            request.ValueInputOption = valueInputOption;
-            request.InsertDataOption = insertDataOption;
-            request.ResponseDateTimeRenderOption = SpreadsheetsResource.ValuesResource.AppendRequest.ResponseDateTimeRenderOptionEnum.FORMATTEDSTRING;
-            AppendValuesResponse response = request.Execute();
+                SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(valueRange, _configService.Config.SpreadsheetLog.Id, range);
+                request.ValueInputOption = valueInputOption;
+                request.InsertDataOption = insertDataOption;
+                request.ResponseDateTimeRenderOption = SpreadsheetsResource.ValuesResource.AppendRequest.ResponseDateTimeRenderOptionEnum.FORMATTEDSTRING;
+                AppendValuesResponse response = request.Execute();
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE ERROR LIST. ERROR: {err.Message}");
+            }
+
         }
 
-        public void LogSendedList(IEnumerable<SendedMessage> messages)
+        public async Task LogSendedList(IEnumerable<SendedMessage> messages)
         {
-            var service = _sheetServiceProvider.GetService().Result;
+            try {
+                var service = await _sheetServiceProvider.GetService();
 
-            var range = $"{_configService.Config.SpreadsheetLog.Messages}!A:D";
-            SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum valueInputOption
-                = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
-            SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum insertDataOption
-                = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
-            ValueRange valueRange = new ValueRange();
+                var range = $"{_configService.Config.SpreadsheetLog.Messages}!A:D";
+                SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum valueInputOption
+                    = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+                SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum insertDataOption
+                    = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+                ValueRange valueRange = new ValueRange();
 
-            var listOfObj = messages.Select(x => (IList<object>) new List<object>() { "Исходящее", string.Format(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")), x.Message, x.To }).ToList();
-            valueRange.Values = listOfObj;//new List<IList<object>> { listOfObj };
+                var listOfObj = messages.Select(x => (IList<object>)new List<object>() { "Исходящее", string.Format(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")), x.Message, x.To }).ToList();
+                valueRange.Values = listOfObj;//new List<IList<object>> { listOfObj };
 
 
-            SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(valueRange, _configService.Config.SpreadsheetLog.Id, range);
-            request.ValueInputOption = valueInputOption;
-            request.InsertDataOption = insertDataOption;
-            request.ResponseDateTimeRenderOption = SpreadsheetsResource.ValuesResource.AppendRequest.ResponseDateTimeRenderOptionEnum.FORMATTEDSTRING;
-            AppendValuesResponse response = request.Execute();
+                SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(valueRange, _configService.Config.SpreadsheetLog.Id, range);
+                request.ValueInputOption = valueInputOption;
+                request.InsertDataOption = insertDataOption;
+                request.ResponseDateTimeRenderOption = SpreadsheetsResource.ValuesResource.AppendRequest.ResponseDateTimeRenderOptionEnum.FORMATTEDSTRING;
+                AppendValuesResponse response = request.Execute();
+            }
+            catch (Exception err)
+            {
+                _toFileLogger.LogError($"CANNOT LOG TO GOOGLE TABLE SENDED LIST. ERROR: {err.Message}");
+            }
         }
     }
 }
