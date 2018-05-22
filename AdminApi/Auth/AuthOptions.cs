b3e67534951880288d211ee424ae12f31faff2a7
Ekaterina Bakuleva <ekaterinabakuleva@mysale.com>
2018-05-22 20:03:32 +0300

    15 min token timeout

--- AdminApi/Auth/AuthOptions.cs
+++ AdminApi/Auth/AuthOptions.cs
@@ -12,7 +12,7 @@ namespace AdminApi.Auth
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "http://localhost:51884/"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 1; // время жизни токена - 1 минута
+        public const int LIFETIME = 15; // время жизни токена - 15 мин
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
